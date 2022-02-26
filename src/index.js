3d7d22d9410e53ab63589e069426f40bd4435946
Jaime Guerrero <JJGuerrero032@Gmail.com>
2022-02-26 14:51:50 -0500

    Updated FrontEnd LinkedIn Clone

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import * as serviceWorker from './serviceWorker';
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <App/>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
@@ -18,4 +18,5 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
+
 serviceWorker.unregister();
